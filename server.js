78a2378a049f13462e9a84549a866ab4133e5262
IwantMcdo <mjhonmarcelo@gmail.com>
2025-03-14 02:08:18 +0800

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes for different pages
+// Serve homepage
 app.get('/', (req, res) => {
-    res.sendFile(path. join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// Serve about and contact pages
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
@@ -20,7 +21,22 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
-// Start the server
+// Serve blog page
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
+});
+
+// API route to fetch blog posts
+app.get('/api/posts', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Failed to load blog posts' });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}` );
+    console.log(`Server running at http://localhost:${PORT}`);
 });
